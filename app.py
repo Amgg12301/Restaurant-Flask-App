f3fa56b058095ec5648380570e77789bb492c1ae
Amogh Giri <amoghgiri01@gmail.com>
2020-03-08 13:22:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ from fusionapi import query_api
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("geolocation.html")
+
 
 @app.route('/results', methods=['GET','POST'])
 def results():
@@ -25,12 +27,14 @@ def results():
         else:
             number_price = 4
         x = location.split(", ")
-        results = query_api(term = type, latitude = x[0], longitude = x[1], price = number_price);
+        latitude = x[0]
+        longitude = x[1]
+        results = query_api(term = type, latitude = latitude, longitude = longitude, price = number_price);
         if not results:
             results = []
-        return render_template('results.html', results = results, price = price)
-    else:
-        return render_template('index.html')
+        return render_template('results.html', results = results, price = price, latitude = latitude, longitude = longitude)
 
-if __name__ == "__main__":
-    app.run()
+    return render_template('geolocation.html')
+
+if __name__ == '__main__':
+   app.run(host = 'localhost', debug = True, port = 5000)
