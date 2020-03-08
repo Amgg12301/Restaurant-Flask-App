4576169cd750e68fb9ea585b166d4c2097d10d8d
Amogh Giri <amoghgiri01@gmail.com>
2020-03-08 17:47:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
+def index():
     return render_template("index.html")
 
 
@@ -37,4 +37,4 @@ def results():
     return render_template('index.html')
 
 if __name__ == '__main__':
-   app.run(host = 'localhost', debug = True, port = 5000)
+   app.run(debug = True, port = 0)
