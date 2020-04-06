57cb962b1d87c8ad46f9229245bf1832b2563e67
Amogh Giri <amoghgiri01@gmail.com>
2020-04-06 01:49:25 -0400

    Coronavirus Update

--- app.py
+++ app.py
@@ -6,34 +6,54 @@ from fusionapi import query_api
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
 
 
-@app.route('/results', methods=['GET','POST'])
+@app.route('/results', methods=['GET', 'POST'])
 def results():
     if request.method == 'POST':
         type = request.form['type']
         location = request.form['location']
         price = request.form['price']
-        if(price == '$'):
+        transactions = []
+
+        if price == '$':
             number_price = 1
-        elif(price == '$$'):
+        elif price == '$$':
             number_price = 2
-        elif(price == '$$$'):
+        elif price == '$$$':
             number_price = 3
         else:
             number_price = 4
+
         x = location.split(", ")
         latitude = x[0]
         longitude = x[1]
-        results = query_api(term = type, latitude = latitude, longitude = longitude, price = number_price);
+        results = query_api(term=type, latitude=latitude, longitude=longitude, price=number_price)
+
         if not results:
             results = []
-        return render_template('results.html', results = results, price = price, latitude = latitude, longitude = longitude)
+
+        for business in results:
+            if 'delivery' in business['transactions'] and 'pickup' in business['transactions']:
+                transactions.append("Delivery and Pickup are both available!")
+            elif 'pickup' in business['transactions']:
+                transactions.append("Only pickup is available!")
+            elif 'delivery' in business['transactions']:
+                transactions.append("Only delivery is available!")
+            else:
+                transactions.append("Both options are unavailable, sorry :(")
+            print(transactions)
+
+        return render_template('results.html', transactions=transactions, results=results,
+                               results_and_transactions=zip(results, transactions), price=price,
+                               latitude=latitude, longitude=longitude)
 
     return render_template('index.html')
 
+
 if __name__ == '__main__':
-   app.run(debug = True, port = 0)
+    app.run(debug=True, port=0)
